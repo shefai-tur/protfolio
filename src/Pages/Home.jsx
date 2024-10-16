2dd6af0593c9a9be564325156e4e92a1160ce132
shefaitur <shefaitur@gmail.com>
2024-10-16 17:31:23 +0600

    update

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import About from "../components/About";
+import Banner from "../components/Banner";
+import Fotter from "../components/Fotter";
+import GetInTouch from "../components/GetInTouch";
+import LatestWorks from "../components/LatestWorks";
+import Sidbar from "../components/Sidbar";
+import Skill from "../components/Skill";
+import Testimonials from "../components/Testimonials";
+const Home = () => {
+  return (
+    <main class="flex flex-col h-screen">
+    <div class="flex flex-1 overflow-hidden">
+      <div class="flex bg-gray-100 w-[140px]p-4">
+        <Sidbar />
+      </div>
+      <div class="flex flex-1 flex-col overflow-y-auto">
+      <Banner />
+      <About />
+      <Skill />
+      <LatestWorks />
+      <Testimonials />
+        <GetInTouch /> 
+      </div>
+    </div>
+    <div>
+
+      <Fotter />
+    </div>
+  </main>
+  )
+}
+
+export default Home
--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -18,103 +18,145 @@ const Skill = () => {
           </div>
           <div className="w-1/2">
             <div className="flex flex-wrap gap-10">
-              <div className="rounded-full w-24 h-24 bg-secondary shadow-lg align-middle">
+              <div className="cursor-pointer text-center rounded-full w-24 h-24  bg-secondary shadow-lg align-middle">
                 <img
                   src="image/skill1.png"
                   alt="Skill Image Alt Text"
                   className=" p-2"
                 />
+                <h1 className=" decoration-slate-400 text-sm font-medium font-popins tracking-widest text-slate-600 hover:text-black ">
+                  Java Script
+                </h1>
               </div>
-              <div className=" rounded-full w-24 h-24 bg-secondary shadow-lg align-middle">
+              <div className="cursor-pointer text-center  rounded-full w-24 h-24 bg-secondary shadow-lg align-middle">
                 <img
                   src="image/skill2.png"
                   alt="Skill Image Alt Text "
-                  className=" p-2"
+                  className="p-2"
                 />
+                <h1 className=" decoration-slate-400 text-sm font-medium font-popins tracking-widest text-slate-600 hover:text-black ">
+                  React Js
+                </h1>
               </div>
-              <div className="rounded-full w-24 h-24 bg-secondary shadow-lg align-middle">
+              <div className="cursor-pointer text-center  rounded-full w-24 h-24 bg-secondary shadow-lg align-middle">
                 <img
                   src="image/skill3.png"
                   alt="Skill Image Alt Text"
                   className=" p-2"
                 />
+                <h1 className=" decoration-slate-400 text-sm font-medium font-popins tracking-widest text-slate-600 hover:text-black ">
+                 Next JS
+                </h1>
               </div>
-              <div className="rounded-full w-24 h-24 bg-secondary shadow-lg align-middle ">
+              <div className="cursor-pointer text-center  rounded-full w-24 h-24 bg-secondary shadow-lg align-middle ">
                 <img
                   src="image/skill4.png"
                   alt="Skill Image Alt Text"
                   className=" p-2"
                 />
+                <h1 className=" decoration-slate-400 text-sm font-medium font-popins tracking-widest text-slate-600 hover:text-black ">
+                 Node Js
+                </h1>
               </div>
-              <div className="rounded-full w-24 h-24 bg-secondary shadow-lg align-middle">
+              <div className="cursor-pointer text-center  rounded-full w-24 h-24 bg-secondary shadow-lg align-middle">
                 <img
                   src="image/skill5.png"
                   alt="Skill Image Alt Text"
                   className=" p-2"
                 />
+                <h1 className=" decoration-slate-400 text-sm font-medium font-popins tracking-widest text-slate-600 hover:text-black ">
+                 MongoDB
+                </h1>
               </div>
-              <div className=" rounded-full w-24 h-24 bg-secondary shadow-lg align-middle">
+              <div className=" cursor-pointer text-center  rounded-full w-24 h-24 bg-secondary shadow-lg align-middle">
                 <img
                   src="image/skill6.png"
                   alt="Skill Image Alt Text "
                   className=" p-2"
                 />
+                <h1 className=" decoration-slate-400 text-sm font-medium font-popins tracking-widest text-slate-600 hover:text-black ">
+                 Express Js
+                </h1>
               </div>
-              <div className="rounded-full w-24 h-24 bg-secondary shadow-lg align-middle">
+              <div className="cursor-pointer text-center  rounded-full w-24 h-24 bg-secondary shadow-lg align-middle">
                 <img
                   src="image/skill7.png"
                   alt="Skill Image Alt Text"
                   className=" p-2"
                 />
+                <h1 className=" decoration-slate-400 text-sm font-medium font-popins tracking-widest text-slate-600 hover:text-black ">
+                  Redux
+                </h1>
               </div>
-              <div className="rounded-full w-24 h-24 bg-secondary shadow-lg align-middle ">
+              <div className="cursor-pointer text-center  rounded-full w-24 h-24 bg-secondary shadow-lg align-middle ">
                 <img
                   src="image/skill8.png"
                   alt="Skill Image Alt Text"
                   className=" p-2"
                 />
+                <h1 className=" decoration-slate-400 text-sm font-medium font-popins tracking-widest text-slate-600 hover:text-black ">
+                Tailwind
+                </h1>
               </div>
-              <div className="rounded-full w-24 h-24 bg-secondary shadow-lg align-middle">
+              <div className="cursor-pointer text-center  rounded-full w-24 h-24 bg-secondary shadow-lg align-middle">
                 <img
                   src="image/skill9.png"
                   alt="Skill Image Alt Text"
                   className=" p-2"
                 />
+                <h1 className=" decoration-slate-400 text-sm font-medium font-popins tracking-widest text-slate-600 hover:text-black ">
+                  Botstrap
+                </h1>
               </div>
-              <div className=" rounded-full w-24 h-24 bg-secondary shadow-lg align-middle">
+              <div className=" cursor-pointer text-center  rounded-full w-24 h-24 bg-secondary shadow-lg align-middle">
                 <img
                   src="image/skill10.png"
                   alt="Skill Image Alt Text "
                   className=" p-2"
                 />
+                <h1 className=" decoration-slate-400 text-sm font-medium font-popins tracking-widest text-slate-600 hover:text-black ">
+                TypeScript  
+                </h1>
               </div>
-              <div className="rounded-full w-24 h-24 bg-secondary shadow-lg align-middle">
+              <div className="cursor-pointer text-center  rounded-full w-24 h-24 bg-secondary shadow-lg align-middle">
                 <img
                   src="image/skill11.png"
                   alt="Skill Image Alt Text"
                   className=" p-2"
                 />
+                <h1 className=" decoration-slate-400 text-sm font-medium font-popins tracking-widest text-slate-600 hover:text-black ">
+               CSS
+                </h1>
               </div>
-              <div className="rounded-full w-24 h-24 bg-secondary shadow-lg align-middle ">
+              <div className="cursor-pointer text-center  rounded-full w-24 h-24 bg-secondary shadow-lg align-middle ">
                 <img
                   src="image/skill12.png"
                   alt="Skill Image Alt Text"
                   className=" p-2"
                 />
+                <h1 className=" decoration-slate-400 text-sm font-medium font-popins tracking-widest text-slate-600 hover:text-black ">
+               Auth
+                </h1>
               </div>
-              <div className="rounded-full w-24 h-24 bg-secondary shadow-lg align-middle ">
+              <div className="cursor-pointer text-center  rounded-full w-24 h-24 bg-secondary shadow-lg align-middle ">
                 <img
                   src="image/skill13.png"
                   alt="Skill Image Alt Text"
                   className=" p-2"
                 />
+                <h1 className=" decoration-slate-400 text-sm font-medium font-popins tracking-widest text-slate-600 hover:text-black ">
+              Git Hub
+                </h1>
               </div>
-              <div className="rounded-full w-24 h-24 bg-secondary shadow-lg align-middle ">
+              <div className="cursor-pointer text-center  rounded-full w-24 h-24 bg-secondary shadow-lg align-middle ">
                 <img
                   src="image/skill14.png"
                   alt="Skill Image Alt Text"
                   className=" p-2"
                 />
+                <h1 className=" decoration-slate-400 text-sm font-medium font-popins tracking-widest text-slate-600 hover:text-black ">
+                 Firebase
+                </h1>
               </div>
             </div>
           </div>
